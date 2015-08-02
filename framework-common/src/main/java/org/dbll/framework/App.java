66a4ce66ae4b1f08eb92e62327e22147519fec3e
铅笔小小生 <308524725@qq.com>
2015-08-02 22:37:09 +0800

    日志系统

--- framework-common/src/main/java/org/dbll/framework/App.java
+++ framework-common/src/main/java/org/dbll/framework/App.java
@@ -1,13 +1,21 @@
 package org.dbll.framework;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+	
+	protected static Logger log = LoggerFactory.getLogger(App.class);
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        
+        log.info("Hello Log!");
     }
 }
